f1dc3a673c4a0bfdb0cf52129f70d42dff5cc998
David_Lane <davidlanemca@gmail.com>
2020-10-09 13:50:46 -0500

    Updates to CSS to include ids

---
+++ script.js
@@ -0,0 +1,17 @@
+const winningSquares = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,4,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+];
+// Returns array of querylist
+const grid = () => Array.from(document.getElementByClassName('grid-item'));
+const grid-itemNumId = () =>
+console.log('Ready to work!');
+const clickFn =()
+//Add eventListeners on all of the grid-item class items to loop through. Added to quadrand element. Listen for click, Run clickFn  
+const enableListeners = () => grid().forEach(_qe1 => _qe1.addEvenetListener('click', clickFn ));
